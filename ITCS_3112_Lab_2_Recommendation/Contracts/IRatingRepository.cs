f8d5b7da808efe815f0d1620495b9beb54c2d073
Qi Ye <qye2@charlotte.edu>
2026-03-01 02:33:17 -0500

    Added more repository/service

---
+++ ITCS_3112_Lab_2_Recommendation/Contracts/IRatingRepository.cs
@@ -0,0 +1,6 @@
+namespace ITCS_3112_Lab_2_Recommendation.Contracts;
+
+public interface IRatingRepository
+{
+    
+}
---
+++ ITCS_3112_Lab_2_Recommendation/Services/RecommendationService.cs
@@ -0,0 +1,8 @@
+using ITCS_3112_Lab_2_Recommendation.Contracts;
+
+namespace ITCS_3112_Lab_2_Recommendation.Services;
+
+public class RecommendationService : IRecommendationService
+{
+    
+}
